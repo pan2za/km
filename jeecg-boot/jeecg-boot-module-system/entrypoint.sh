a03b0c9f66991b981f024a4adaa368dfe6575165
zhuzhenglu <zhenglu.zhu@coconet.cn>
2025-01-26 17:47:12 +0800

    change waiting es task to curl

--- jeecg-boot/jeecg-boot-module-system/entrypoint.sh
+++ jeecg-boot/jeecg-boot-module-system/entrypoint.sh
@@ -3,12 +3,21 @@
 # 设置最大尝试次数和等待时间（秒）
 MAX_RETRIES=30
 WAIT_TIME=5
+PASSWORD=${OPENSEARCH_INITIAL_ADMIN_PASSWORD}
 
 echo "Checking if kykms-ES is reachable..."
 
+# 定义其他变量
+USERNAME="admin"
+HOST="http://kykms-ES:9200"
+ENDPOINT="/_index_template/km_doc"
+
+# 使用curl进行HEAD请求并捕获状态码
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" -u "${USERNAME}:${PASSWORD}" -I "${HOST}${ENDPOINT}")
+
 for i in $(seq 1 $MAX_RETRIES); do
   # 尝试 ping kykms-ES
-  if ping -c 1 kykms-ES &> /dev/null; then
+  if [ "$STATUS_CODE" -eq 200 ]; then
     echo "kykms-ES is up!"
     break
   else
